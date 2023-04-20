e7fd67e8cffeef3c571f5fa6a88a347954964dd4
tom7438 <tom192010@live.fr>
2023-04-20 15:08:32 +0000

    Ajout prog principal

---
+++ Gaufre.java
@@ -0,0 +1,9 @@
+import Modele.Jeu;
+//import Vue.InterfaceMenu;
+
+public class Gaufre {
+    public static void main(String[] args) throws Exception {
+        Jeu J = new Jeu();
+        //InterfaceMenu.demarrer(J);
+    }
+}
