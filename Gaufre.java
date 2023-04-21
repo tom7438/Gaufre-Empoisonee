6aee701f7c5a5670c141b209f8b3f654cbd33353
tom7438 <tom192010@live.fr>
2023-04-21 08:06:10 +0000

    Ajout possibilité de rejouer une partie

--- Gaufre.java
+++ Gaufre.java
@@ -10,14 +10,21 @@ import java.util.Scanner;
 
 public class Gaufre {
     public static void main(String[] args) throws Exception {
-        // Demander le nombre de lignes et de colonnes
-        Scanner Scanner = new Scanner(System.in);
-        System.out.println("Entrez le nombre de lignes");
-        int nblignes = Scanner.nextInt();
-        System.out.println("Entrez le nombre de colonnes");
-        int nbcolonnes = Scanner.nextInt();
-        Jeu j = new Jeu(nblignes, nbcolonnes);
-        CollecteurEvenements control = new ControleurMediateur(j);
-        //InterfaceMenu.demarrer(J,control);
+        boolean rejouer = true;
+        while(rejouer) {
+            // Demander le nombre de lignes et de colonnes
+            Scanner Scanner = new Scanner(System.in);
+            System.out.println("Entrez le nombre de lignes");
+            int nblignes = Scanner.nextInt();
+            System.out.println("Entrez le nombre de colonnes");
+            int nbcolonnes = Scanner.nextInt();
+            Jeu j = new Jeu(nblignes, nbcolonnes);
+            CollecteurEvenements control = new ControleurMediateur(j);
+            //InterfaceMenu.demarrer(J,control);
+            System.out.println("Voulez-vous rejouer ? (O/N)");
+            char res = Scanner.hasNext() ? Scanner.next().charAt(0) : 'N';
+            rejouer = (res == 'O' || res == 'o');
+        }
+        System.out.println("Au revoir !");
     }
 }
