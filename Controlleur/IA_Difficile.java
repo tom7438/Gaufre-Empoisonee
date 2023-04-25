785ce51168c6d8ae92274248c3a35cea65edac51
4ppolyon <romalves70@gmail.com>
2023-04-25 10:40:43 +0200

    ia proba en cours

--- Controlleur/IA_Difficile.java
+++ Controlleur/IA_Difficile.java
@@ -1,77 +1,140 @@
 package Controlleur;
 
 import Modele.Niveau;
-import java.util.Random;
+import java.util.LinkedList;
+//faire une ia probabiliste
 
+public class IA_Difficile extends Joueur {
+    public static class arbre_naire{
+        int n_ligne;
+        int n_colonne;
+        int nb_coup;
+        float proba_victoire;
+        int[][] plateau;
+        LinkedList<arbre_naire> fils;
+        arbre_naire(int n_ligne, int n_colonne, float proba_victoire, int nb_coup, int[][] plateau){
+            this.n_ligne = n_ligne;
+            this.n_colonne = n_colonne;
+            this.proba_victoire = proba_victoire;
+            this.nb_coup = nb_coup;
+            fils = new LinkedList<>();
+            this.plateau = plateau;
+        }
+    }
 
-public class IA_Difficile extends Joueur{
-
-    Random r;
+    arbre_naire racine;
 
     IA_Difficile(int n, Niveau p) {
         super(n, p);
-        r = new Random();
+    }
+
+    void remplir_arbre(arbre_naire a){
+        arbre_naire fils;
+        //cas ou on a une feuille
+        if(a.plateau[a.n_ligne][a.n_colonne] == 2){
+            if(a.nb_coup % 2 == 0){
+                a.proba_victoire = 1;
+            }else{
+                a.proba_victoire = 0;
+            }
+        //cas ou on a un noeud
+        } else {
+            for (int l = 0; l < niveau.lignes; l++) { //on parcourt le plateau
+                for (int c = 0; c < niveau.colonnes; c++) {
+
+                    if (a.plateau[l][c] == 1) { //si on trouve un morceau
+                        fils = new arbre_naire(l, c, 0, a.nb_coup + 1, a.plateau); //on cree un nouveau noeud
+
+
+                        if(a.n_ligne < fils.n_ligne){ //on regarde si le morceau est a droite ou a gauche
+                            for(int x = a.n_ligne; x <= fils.n_ligne; x++){
+                                if(a.n_colonne < fils.n_colonne) { //on regarde si le morceau est en haut ou en bas
+                                    for (int y = a.n_colonne; y <= fils.n_colonne; y++) {
+                                        if (a.plateau[x][y] == 1) { //on enleve les morceaux entre les deux
+                                            fils.plateau[x][y] = 0; //on enleve les morceaux entre les deux
+                                        }
+                                    }
+                                }else{
+                                    for (int y = a.n_colonne; y >= fils.n_colonne; y--) {
+                                        if (a.plateau[x][y] == 1) { //on enleve les morceaux entre les deux
+                                            fils.plateau[x][y] = 0; //on enleve les morceaux entre les deux
+                                        }
+                                    }
+                                }
+                            }
+                        }else{
+                            for (int x = a.n_ligne; x >= fils.n_ligne; x--) {
+                                if(a.n_colonne < fils.n_colonne) {
+                                    for (int y = a.n_colonne; y <= fils.n_colonne; y++) {
+                                        if (a.plateau[x][y] == 1) {
+                                            fils.plateau[x][y] = 0;
+                                        }
+                                    }
+                                }else{
+                                    for (int y = a.n_colonne; y >= fils.n_colonne; y--) {
+                                        if (a.plateau[x][y] == 1) {
+                                            fils.plateau[x][y] = 0;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        a.fils.add(fils);
+                        remplir_arbre(fils);
+                        //TODO :mise a jour des valeurs
+                    }
+                }
+            }
+        }
+    }
+
+    //fonction qui remonte l'arbre en mettant la valeur
+    void evaluer_noeud(arbre_naire a) {
+        if (a.fils.isEmpty()) { // si le nœud est une feuille
+            if (a.nb_coup % 2 == 0) { // si c'est le tour de l'IA
+                a.proba_victoire = 0; // assigner une valeur minimale
+            } else { // si c'est le tour de l'adversaire
+                a.proba_victoire = 1; // assigner une valeur maximale
+            }
+        } else { // si le nœud n'est pas une feuille
+            float max_valeur = -1;
+            for (arbre_naire fils : a.fils) { // parcourir les fils du nœud
+                evaluer_noeud(fils); // évaluer le fils
+                if (fils.proba_victoire > max_valeur) { // si le fils a une valeur supérieure à la valeur maximale actuelle
+                    max_valeur = fils.proba_victoire; // mettre à jour la valeur maximale
+                }
+            }
+            a.proba_victoire = 1 - max_valeur; // assigner la valeur du nœud en fonction de la stratégie de l'IA
+        }
     }
 
     @Override
-    boolean jeu(int num){
-        int i,j;
-
-        int nb_lignes = 0, nb_colonnes =0;
-
-        System.out.println("Numero de coup " +num);
-        if (niveau.getLigne() == niveau.getColonne()){
-            if(num == 1){
-                i = 1;
-                j = 1;
-                niveau.jouer(i,j); 
-                return true;
-            }
-            else if(num == 2) {
-                if(niveau.aMorceau(1, 1)){
-                    niveau.jouer(1,1); 
-                    return true;
+    boolean jeu(int num) {
+        int[][] plateau = new int[niveau.lignes][niveau.colonnes];
+        for (int i = 0; i < niveau.lignes; i++) {
+            for (int j = 0; j < niveau.colonnes; j++) {
+                if (niveau.aMorceau(i, j)) {
+                    plateau[i][j] = 1; 
                 }
-                num =3;
-            }
-            if (num >= 3){
-                //System.out.println("Nombre de lignes " +(niveau.getLigne() -1));
-                for(i=niveau.getLigne() -1; i!=0; i--){
-                    if (niveau.aMorceau(i, 0)){
-                        nb_lignes = i; //ligne max avec un morceau;
-                        break;
-                    }
+                else {
+                    plateau[i][j] = 0;
                 }
-
-                for(i=niveau.getColonne()-1; i!=0; i--){
-                    if (niveau.aMorceau(0, i)){
-                        nb_colonnes = i;
-                        break;
-                    }
-                }
-
-                if (nb_lignes > nb_colonnes){
-                    System.out.println("L'IA difficile a joué le coup (" + (nb_colonnes + 1) + "," + 0 +")" );
-                    niveau.jouer(nb_colonnes +1, 0);
-                }
-                else if (nb_lignes < nb_colonnes){
-                    System.out.println("L'IA difficile a joué le coup (" + 0 + "," + (nb_lignes + 1) +")");
-                    niveau.jouer(0, nb_lignes +1);
-                }
-                else{
-                    i = r.nextInt(2);
-                    if (i == 0){
-                        System.out.println("L'IA difficile a joué le coup (" + 0 + "," + nb_lignes +")");
-                        niveau.jouer(0, nb_lignes );
-                    }
-                    else{
-                        System.out.println("L'IA difficile a joué le coup (" + 0 + "," + nb_colonnes +")");
-                        niveau.jouer(nb_colonnes, 0);
-                    }
-                }
-                return true;
             }
         }
-        return false;
+        plateau[0][0] = 2;
+        racine = new arbre_naire(niveau.lignes - 1, niveau.colonnes - 1, 1, 0, plateau);
+        remplir_arbre(racine);
+        evaluer_noeud(racine);
+        int i =0 , j = 0, ok;
+        float max = -1;
+        for(int x = 0; x < racine.fils.size(); x++){
+            if(racine.fils.get(x).proba_victoire > max){
+                max = racine.fils.get(x).proba_victoire;
+                i = racine.fils.get(x).n_ligne;
+                j = racine.fils.get(x).n_colonne;
+            }
+        }
+        ok = niveau.jouer(i,j);
+        return ok == 0;
     }
 }
